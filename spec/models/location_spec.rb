e9373bbef752a3d7c49783bc0a381b90032879a5
Brad Cook <cookbrad@oregonstate.edu>
2018-04-09 15:10:14 -0700

    Change declaration of location to a symbol

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,14 +1,13 @@
 require "rails_helper"
 
 RSpec.describe Location do
+  let (:location) {Location.new("london", 51.509865, -0.118092)}
   it "knows a list of points of interest" do
-    location = Location.new("london", 51.509865, -0.118092)
     location.set_point_of_interest("Big Ben")
     expect(location.get_points_of_interest).to match_array(["Big Ben"])
   end
 
   it "has latitude-longitude coordinates" do
-    location = Location.new("london", 51.509865, -0.118092)
     expect(location.get_coordinates).not_to be_empty
   end
 end
