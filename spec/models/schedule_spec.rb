1baa33b60aefa0995a3be9c376da560b65471476
gonzalky7 <gonzalky@oregonstate.edu>
2018-04-09 15:18:39 -0700

    Refactor schedule_spec.rb

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -1,14 +1,13 @@
 require "rails_helper"
 
 RSpec.describe Schedule do
-  
+  let (:schedule) {Schedule.new()}
+
   it "considers a scheule with no acitivies to be done" do
-  schedule = Schedule.new
   expect(schedule.done?).to be_truthy
   end
 
   it "knows that a schedule with an incomplete activity is not done" do
-    schedule = Schedule.new
     activity = Activity.new
     schedule.activities << activity
     expect(schedule.done?).to be_falsy
