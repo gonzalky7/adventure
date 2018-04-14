4ff4f1345c8e71f351ef2741ddb06d290f55813e
gonzalky7 <gonzalky@oregonstate.edu>
2018-04-14 16:26:27 -0700

    Ughh

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411204825) do
-
-  create_table "locations", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
