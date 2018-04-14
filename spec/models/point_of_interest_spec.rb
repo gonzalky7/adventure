c500b3155cc8e5b073de28e4035aee425cb8faae
gonzalky7 <gonzalky@oregonstate.edu>
2018-04-14 16:26:27 -0700

    Ughh

--- spec/models/point_of_interest_spec.rb
+++ spec/models/point_of_interest_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe PointOfInterest do 
-  let (:poi) {PointOfInterest.new("Big Ben", 51.5007, - 0.1246)}
+  let (:point_of_interest) {PointOfInterest.new("Big Ben", 51.5007, - 0.1246)}
 
   it "knows that a poi should has a name" do
-    expect(poi.has_name?).to be_truthy
+    expect(point_of_interest.has_name?).to be_truthy
   end
 
 
   it "has latitude-longitude coordinates" do
-    expect(poi.get_coordinates).not_to be_empty
+    expect(point_of_interest.get_coordinates).not_to be_empty
   end
 
 end
--- spec/system/add_point_of_interest_spec.rb
+++ spec/system/add_point_of_interest_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-# RSpec.describe "adding a point of interenest", type: :system do
-# 	it "allows a user to create a point of interenst" do
-# 	visit new_point_of_interest_path
-# 	fill_in "Name", with: "Giza pyramids"
-# 	fill_in "Logitude", with: "29.9792"
-# 	fill_in "Latitude", with: "31.1342"
-# 	click_on ("Create Point Of Interest")
-# 	visit point_of_interest_path 
-# 	expect(page).to have_content("Giza pyramids")
-# 	expect(page).to have_content("29.9792") 
-# 	end
-# end
+RSpec.describe "adding a point of interenest", type: :system do
+	it "allows a user to create a point of interenst" do
+	visit new_point_of_interest_path
+	fill_in "Name", with: "Giza pyramids"
+	fill_in "Logitude", with: "29.9792"
+	fill_in "Latitude", with: "31.1342"
+	click_on ("Create Point Of Interest")
+	visit point_of_interest_path 
+	expect(page).to have_content("Giza pyramids")
+	expect(page).to have_content("29.9792") 
+	end
+end
