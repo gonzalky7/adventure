e9ce7faee675c577269934c1809ce11e3e3b134a
Brad Cook <cookbrad@oregonstate.edu>
2018-04-15 20:14:44 -0700

    Add system test for add loction

--- spec/system/add_location_spec.rb
+++ spec/system/add_location_spec.rb
@@ -2,12 +2,13 @@ require "rails_helper"
 
  RSpec.describe "adding a location", type: :system do
    it "allows a user to add a location to their itinerary" do
-     # visit new_project_path
-     # fill_in "Name", with: "Project Runway"
-     # fill_in "Tasks", with: "Choose Fabric:3\nMake it Work:5"
-     # click_on("Create Project")
-     # visit projects_path
-     # expect(page).to have_content("Project Runway")
-     # expect(page).to have_content(8)
+     visit new_location_path
+     fill_in "Name", with: "London"
+     fill_in "Latitude", with: 51.509865
+     fill_in "Longitude", with: -0.118092
+     click_on("Create Location")
+     visit projects_path
+     expect(page).to have_content("London")
+     expect(page).to have_content(51.509865)
    end
  end
