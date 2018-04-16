853059f95a468dd636042520fe7895f4ead63cda
Brad Cook <cookbrad@oregonstate.edu>
2018-04-16 12:36:13 -0700

    Define create and index in locations controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,14 @@
 class LocationsController < ApplicationController
   def new
-    @location = Location.new
+    @locations = Location.new
+  end
+  def index
+    @Locations = Location.all
+  end
+
+  def create
+    @workflow = Adds.new(name: params[:location][:name])
+    @workflow.create
+    redirect_to locations_path
   end
 end
