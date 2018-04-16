9cf7167533738cbc63d23c06d448c60b67a2e5a3
Brad Cook <cookbrad@oregonstate.edu>
2018-04-15 20:35:33 -0700

    Add a workflow adds location

--- spec/workflows/add_location_spec.rb
+++
@@ -1,8 +0,0 @@
-require "rails_helper"
-RSpec.describe AddLocation do
-  it "adds a location given a name" do
-    adder = AddLocation.new(name: "London")
-    adder.build
-    expect(adder.project.name).to eq("London")
-  end
-end
