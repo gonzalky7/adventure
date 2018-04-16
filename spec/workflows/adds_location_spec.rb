8d7931bc5105cda24c97a56573f7218475e74d99
Brad Cook <cookbrad@oregonstate.edu>
2018-04-16 15:07:36 -0700

    Change submit button label

--- spec/workflows/adds_location_spec.rb
+++ spec/workflows/adds_location_spec.rb
@@ -3,6 +3,6 @@ RSpec.describe AddsLocation do
   it "adds a location given a name" do
     adder = AddsLocation.new(name: "London")
     adder.build
-    expect(adder.project.name).to eq("London")
+    expect(adder.location.name).to eq("London")
   end
 end
