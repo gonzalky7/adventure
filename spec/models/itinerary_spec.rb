5403d3174254c37ec29be81bcaa30bf6a59f51ae
gonzalky7 <gonzalky@oregonstate.edu>
2018-04-27 22:40:57 -0700

    Test: Return highest rated location in itinerary

--- spec/models/itinerary_spec.rb
+++ spec/models/itinerary_spec.rb
@@ -32,9 +32,37 @@ RSpec.describe Itinerary do
     expect(itinerary.current_location.name).to eq "London"
   end
 
-
   it "knows its next location" do
     itinerary.next_location << location_2
   end
+
+  describe "ratings" do
+    let (:location_1) { Location.new(
+    name: "London",
+    latitude: 51.509865,
+    longitude: -0.118092,
+    rating:10)
+    }
+    let (:location_2) { Location.new(
+      name: "Bath",
+      latitude: 51.509865,
+      longitude: -0.118092,
+      rating: 5)
+    }
+    let (:location_3) { Location.new(
+      name: "Kingston",
+      latitude: 51.4123,
+      longitude: -0.3007,
+      rating: 1)
+    }
+
+
+    it "knows what location has the highest rating" do
+      locations = [location_1, location_2, location_3]
+      expect(locations).to be_highest_rating(10)
+      expect(locations).not_to be_highest_rating(1)
+    end
+  end
+
 
 end
