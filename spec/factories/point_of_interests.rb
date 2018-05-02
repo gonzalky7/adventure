ce502d41fe8469ad111ffd53d7962887aa46fe31
gonzalky7 <gonzalky@oregonstate.edu>
2018-05-02 15:24:31 -0700

    Creating factory definition point of interests

---
+++ spec/factories/point_of_interests.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :point_of_interest do
+    name "Great Barrier Reef"
+    longitude 1345.45
+    latitude 51.5007
+    description "Just Keep Swimming"
+  end
+end
+
--- spec/models/point_of_interest_spec.rb
+++ spec/models/point_of_interest_spec.rb
@@ -1,21 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe PointOfInterest do 
-  let (:point_of_interest) { PointOfInterest.new(
-    name: "Big Ben",
-    latitude: 51.5007,
-    longitude: 0.1246,
-    description: "Big Ben is the nickname for the Great Bell of the clock in London"
-    )
-  }
+  let (:point_of_interest) { FactoryBot.build(:point_of_interest)} 
 
   it "knows that a point of interest should has a name" do
-    expect(point_of_interest.name).to eq("Big Ben")
+    expect(point_of_interest.name).to eq("Great Barrier Reef")
   end
 
   it "has latitude-longitude coordinates" do
     expect(point_of_interest.latitude).to eq(51.5007)
-    expect(point_of_interest.longitude).to eq(0.1246)
+    expect(point_of_interest.longitude).to eq(1345.45)
   end
 
   it "has a description of point of interest" do
